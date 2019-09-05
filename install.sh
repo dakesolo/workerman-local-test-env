ec311f8d42d65726cd38b35c49c4182ab2439dfc
qissen <569072171@qq.com>
2019-09-05 08:45:01 +0800

    脚本完善

---
+++ install.sh
@@ -0,0 +1,11 @@
+yum -y install yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce-18.06.1.ce-3.el7
+systemctl enable docker && systemctl start docker
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+systemctl stop firewalld.service
+systemctl disable firewalld.service
+docker login --username=original_dk@163.com registry.cn-hangzhou.aliyuncs.com
+docker pull registry.cn-hangzhou.aliyuncs.com/dake/php:v1
+docker run --name workerman -p 80:5001 -v /home/wwwroot:/var/www -d dake/php:v1 /bin/bash start.sh
